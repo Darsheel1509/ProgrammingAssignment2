28917ac09d0e1dfecf1fb41fe0e1b2678649ce51
Darsheel1509 <34442886+Darsheel1509@users.noreply.github.com>
2017-12-11 17:03:31 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,14 @@ makeCacheMatrix <- function(x = matrix()) {
   c <- NULL
   set <- function(y) {
     x <<- y
-    c <<- NULL
+    c <<- NULL  ## ASSIGNING VALUE OUTSIDE THE ENVIRONMENT
   }
-  get <- function() x
-  setting_inverse <- function(inverse) c <<- inverse
+  get <- function() x 
+  setting_inverse <- function(inverse) c <<- inverse     
   getting_inverse <- function() c
   list(set = set, get = get,
        setting_inverse = setting_inverse,
-       getting_inverse = getting_inverse)
+       getting_inverse = getting_inverse)      
 
 }
 
@@ -20,11 +20,11 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   c <- x$getting_inverse()
   if(!is.null(c)) {
-    message("getting the cached data")
+    message("getting the cached data") ##CHECKING IF THE INVERSE ALREADY BEEN CALCULATED
     return(c)
   }
   data <- x$get()
-  c <- solve(data, ...)
+  c <- solve(data, ...)  ## INVERSE BEING CALCULATED USING SOLVE
   x$set(c)
   c
 }
